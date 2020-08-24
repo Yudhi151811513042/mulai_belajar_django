c50db8f284228a94e15873b3b28cf4877bcfd112
Yudhi151811513042 <yudhiarnantorangga@gmail.com>
2020-08-24 13:48:25 +0700

    konsep app di django

--- blog_site/blog_site/urls.py
+++ blog_site/blog_site/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from . import views
 
 urlpatterns = [
+    path('blogs/', include('blogs.urls')),
     path('', views.hello_bosku),
     path('admin/', admin.site.urls),
 ]
