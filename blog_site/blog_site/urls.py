cc96ffdd59076933cddbeb0799275b4b373cd78c
Yudhi151811513042 <yudhiarnantorangga@gmail.com>
2020-08-25 14:02:00 +0700

    static file css django

--- blog_site/blog_site/urls.py
+++ blog_site/blog_site/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('blogs/', include('blogs.urls')),
     path('', views.welcome),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
