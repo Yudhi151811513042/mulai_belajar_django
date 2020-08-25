26dae041140f41b46b944a76eb15c057198cfa20
Yudhi151811513042 <yudhiarnantorangga@gmail.com>
2020-08-25 15:13:29 +0700

    model dan admin sistem komentar

--- blog_site/blogs/admin.py
+++ blog_site/blogs/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Blog
-# Register your models here.
+from .models import Blog, Comment
+# Register your models her(object):
+
 admin.site.register(Blog)
+admin.site.register(Comment)
