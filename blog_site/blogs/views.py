16e1a4e3ab7f26462dccbf034be8ae87ccf9a6ed
Yudhi151811513042 <yudhiarnantorangga@gmail.com>
2020-08-31 10:48:04 +0700

    form komentar untuk user

--- blog_site/blogs/views.py
+++ blog_site/blogs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Blog
 
 def handler404(request):
@@ -13,4 +13,12 @@ def  single(request, id):
     blog = get_object_or_404(Blog, pk = id)
     return render(request, 'blogs/single.html', {'blog': blog})
 
+def  comment(request, id):
+
+    blog = get_object_or_404(Blog, pk = id)
+
+    if request.method == 'POST':
+        newDesc = request.POST['desc']
+        blog.comment_set.create(desc=newDesc)
+        return HttpResponseRedirect('/blogs')
 # Create your views here.
