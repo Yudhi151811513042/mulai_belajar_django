d1b57b611670eef153551b1805ecdc62e34cb6af
Yudhi151811513042 <yudhiarnantorangga@gmail.com>
2020-08-31 14:00:45 +0700

    validasi error

--- blog_site/blog_site/forms.py
+++ blog_site/blog_site/forms.py
@@ -1,6 +1,9 @@
 from django import forms
+from django.core import validators
 
 class ContactForm(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     subject = forms.CharField(widget=forms.Textarea)
+    honeypot = forms.CharField(widget=forms.HiddenInput, required=False,
+                                validators=[validators.MaxLengthValidator(0)])
